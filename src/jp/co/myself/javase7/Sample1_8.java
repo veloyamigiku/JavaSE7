9365f9b8ebfdb5729611be2ebad3f746af3d1885
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-19 15:42:41 +0900

    final修飾子を使用した変数の定義の、サンプルを追加します。

---
+++ src/jp/co/myself/javase7/Sample1_8.java
@@ -0,0 +1,22 @@
+package jp.co.myself.javase7;
+
+class Foo {
+	final int num1 = 10;
+	final int num2;
+	Foo(int i) {
+		num2 = i;
+	}
+}
+
+public class Sample1_8 {
+	
+	public static void main(String[] args) {
+		final Foo obj1 = new Foo(100);
+		// 以下のコメント文は、コンパイルエラーの文です。
+		//obj1.num1 = 20;
+		//obj1 = new Foo(300);
+		Foo obj2 = new Foo(300);
+		System.out.println("obj1.num:" + obj1.num1);
+		System.out.println("obj2.num:" + obj2.num2);
+	}
+}
