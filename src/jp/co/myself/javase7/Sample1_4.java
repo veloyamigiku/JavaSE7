c04f59ea03d327b86803c2b55241382ff60d10b1
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-19 15:19:04 +0900

    Switch文のサンプル4を追加します。

---
+++ src/jp/co/myself/javase7/Sample1_4.java
@@ -0,0 +1,23 @@
+package jp.co.myself.javase7;
+
+public class Sample1_4 {
+	
+	public static void main(String[] args) {
+		String signal = args[0];
+		switch (signal) {
+		case "red":
+			System.out.println("赤");
+			break;
+		case "yellow":
+			System.out.println("黄");
+			break;
+		case "blue":
+			System.out.println("青");
+			break;
+		default:
+			System.out.println("その他");
+			break;
+		}
+	}
+	
+}
