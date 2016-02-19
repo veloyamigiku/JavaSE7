1df3ded2137f512db372769ac7c48cfae2e52835
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-19 15:20:04 +0900

    進数のサンプルコードを追加します。 ※JDK7から2進数のリテラルをサポートされる。

---
+++ src/jp/co/myself/javase7/Sample1_5.java
@@ -0,0 +1,23 @@
+package jp.co.myself.javase7;
+
+public class Sample1_5 {
+	
+	public static void main(String[] args) {
+		// 10進数の値を変数に格納します。
+		int val1 = 26;
+		// 8進数の値を変数に格納します。
+		int val2 = 032;
+		// 16進数の値を変数に格納します。
+		int val3 = 0x1a;
+		// 2進数の値を変数に格納します。
+		int val4 = 0b11010;
+		// 2進数の値を変数に格納するエラーパターンです。
+		//int val5 = 0b1201;
+		
+		System.out.println("val1:" + val1);
+		System.out.println("val2:" + val2);
+		System.out.println("val3:" + val3);
+		System.out.println("val4:" + val4);
+	}
+	
+}
