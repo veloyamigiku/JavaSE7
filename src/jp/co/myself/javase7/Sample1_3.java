c5b8c9a51fd6e17e3a8d48c98e895d5d97050fb4
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-19 11:54:05 +0900

    Switch文のサンプル3を追加します。

---
+++ src/jp/co/myself/javase7/Sample1_3.java
@@ -0,0 +1,18 @@
+package jp.co.myself.javase7;
+
+public class Sample1_3 {
+	
+	static Integer num;
+	
+	public static void main(String[] args) {
+		switch (num) {
+		case 0:
+			System.out.println(0);
+			break;
+		case 1:
+			System.out.println(1);
+			break;
+		}
+	}
+	
+}
