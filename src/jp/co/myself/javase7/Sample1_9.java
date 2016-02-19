6ed4717f95b64d22e91ff7ebd312a7583aff2e6a
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-19 15:51:02 +0900

    staticメンバのクラス内でのアクセスの、サンプルを追加します。

---
+++ src/jp/co/myself/javase7/Sample1_9.java
@@ -0,0 +1,21 @@
+package jp.co.myself.javase7;
+
+public class Sample1_9 {
+	
+	int instanceVal;
+	static int staticVal;
+	
+	int methodA() { return instanceVal; }
+	int methodB() { return staticVal; }
+	// 以下のコメント文は、コンパイルエラーの文です。
+	//static int methodC() { return instanceVal; }
+	static int methodD() { return staticVal; }
+	static int methodE() {
+		Sample1_9 obj = new Sample1_9();
+		return obj.instanceVal;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
