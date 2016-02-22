d5443a7d50f6ff996989740b37e41932a795ca32
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-22 14:46:37 +0900

    staticインポートのサンプルを追加します。

---
+++ src/jp/co/myself/javase7/Sample1_19.java
@@ -0,0 +1,14 @@
+package jp.co.myself.javase7;
+
+import static java.lang.System.out;
+import static java.lang.Math.random;
+
+public class Sample1_19 {
+	
+	public static void main(String[] args) {
+		out.println(random());
+		
+		System.out.println(Math.random());
+	}
+	
+}
