169c0223d43bb172e12c96fde394734e17df55d4
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-22 11:48:50 +0900

    equalsメソッドの利用サンプルを追加します。

---
+++ src/jp/co/myself/javase7/Sampel1_16.java
@@ -0,0 +1,17 @@
+package jp.co.myself.javase7;
+
+class Foo6 {
+	
+}
+
+public class Sampel1_16 {
+	public static void main(String[] args) {
+		Foo6 f1 = new Foo6();
+		Foo6 f2 = new Foo6();
+		System.out.println("f1.equals(f2):" + f1.equals(f2));
+		
+		Foo6 f3 = new Foo6();
+		Foo6 f4 = f3;
+		System.out.println("f3.equals(f4):" + f3.equals(f4));
+	}
+}
