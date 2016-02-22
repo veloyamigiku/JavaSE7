62f075a04b7023d42b4c84061cd3a4a0cfe9ee80
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-22 11:45:17 +0900

    finalizeメソッドの再定義サンプルを追加します。

---
+++ src/jp/co/myself/javase7/Sample1_15.java
@@ -0,0 +1,22 @@
+package jp.co.myself.javase7;
+
+class Foo5 {
+	
+	@Override
+	protected void finalize() throws Throwable {
+		
+	}
+	
+}
+
+class Bar2 extends Foo5 {
+	
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+	}
+	
+}
+public class Sample1_15 {
+
+}
