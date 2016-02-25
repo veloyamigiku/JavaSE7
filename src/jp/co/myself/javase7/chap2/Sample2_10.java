491a14a3083f52f8344928a1499eac90c6b7d736
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-25 11:57:52 +0900

    インタフェースの定義・実装のサンプルを追加します。

---
+++ src/jp/co/myself/javase7/chap2/Sample2_10.java
@@ -0,0 +1,23 @@
+package jp.co.myself.javase7.chap2;
+
+interface MyInter1 {
+	void methodA();
+}
+
+class Super4 {
+	void methodB() {
+		
+	}
+}
+
+public class Sample2_10 extends Super4 implements MyInter1 {
+	
+	@Override
+	public void methodA() {
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
