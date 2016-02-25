052577fb58d684672f2890618741a4ac6c8bbd82
velo.yamigiku <velo.yamigiku@gmail.com>
2016-02-25 11:52:59 +0900

    コンパイルできないインタフェースの定義サンプルを追加します。

---
+++ src/jp/co/myself/javase7/chap2/Sample2_9.java
@@ -0,0 +1,21 @@
+package jp.co.myself.javase7.chap2;
+
+interface MyInter {
+	
+	// インスタンス変数は定義不可です。
+	//int a;
+	
+	// public static以外のメソッド定義は不可です。
+	//protected void methodA();
+	//final void methodB();
+	//static void methodC();
+	
+}
+
+public class Sample2_9 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
