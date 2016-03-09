2ad5dfe2a81ac333fb84ffac45e5b57ea3fb0e65
velo.yamigiku <velo.yamigiku@gmail.com>
2016-03-09 10:37:32 +0900

    ネストクラス定義のサンプルを追加します。

---
+++ src/jp/co/myself/javase7/chap3/Sample3_1.java
@@ -0,0 +1,18 @@
+package jp.co.myself.javase7.chap3;
+
+class Outer {
+	class A {
+		
+	}
+	static class B {
+		
+	}
+}
+
+public class Sample3_1 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
