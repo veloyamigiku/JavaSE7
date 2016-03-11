d0b5e23b56fdaf1311d173a5ff20096eda1b18b9
velo.yamigiku <velo.yamigiku@gmail.com>
2016-03-11 10:03:03 +0900

    ファクトリパターン(xxx.valueOf)のサンプルを追加します。

---
+++ src/jp/co/myself/javase7/chap4/Sample4_10.java
@@ -0,0 +1,11 @@
+package jp.co.myself.javase7.chap4;
+
+public class Sample4_10 {
+	
+	public static void main(String[] args) {
+		Integer obj1 = Integer.valueOf("100");
+		Boolean obj2 = Boolean.valueOf("true");
+		System.out.println(obj1 + ": " + obj2);
+	}
+	
+}
