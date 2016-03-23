48a5e41b57b4e8853816da07a260f9b90272684c
velo.yamigiku <velo.yamigiku@gmail.com>
2016-03-23 11:37:36 +0900

    マルチキャッチのサンプルを追加します。

---
+++ src/jp/co/myself/javase7/chap7/Sample7_4.java
@@ -0,0 +1,23 @@
+package jp.co.myself.javase7.chap7;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Sample7_4 {
+	public static void main(String[] args) {
+		try {
+			int a = 10 / 0;
+			FileReader rf = new FileReader(".gitignore");
+			rf.read();
+		} catch (ArithmeticException | FileNotFoundException e) {
+			e.printStackTrace();
+			// 参照変数への再代入は出来ません。
+			//e = null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			// 参照変数への再代入は出来ません。
+			//e = null;
+		}
+	}
+}
